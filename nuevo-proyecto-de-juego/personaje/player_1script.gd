9fc58256d73169f7e10f17fe0fd28ed9a3971089
Leonardo Maidana <leo-lomas1@hotmail.com>
2026-04-29 21:29:14 -0300

    Implementación del doble salto.

--- nuevo-proyecto-de-juego/personaje/player_1script.gd
+++ nuevo-proyecto-de-juego/personaje/player_1script.gd
@@ -2,19 +2,22 @@ extends CharacterBody2D
 
 
 const SPEED = 400.0
-const JUMP_VELOCITY = -700.0
-
+const JUMP_VELOCITY = -500.0
+var  max_jumps = 2
+var jumps_left = 2
 
 func _physics_process(delta: float) -> void:
 
-	if not is_on_floor():
+	if !is_on_floor():
 		velocity += get_gravity() * delta
-
-
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-
+		
+	if Input.is_action_just_pressed("ui_up") and jumps_left > 0:
+			velocity.y = JUMP_VELOCITY
+			jumps_left -= 1
+			
+	if is_on_floor():
+		jumps_left = max_jumps
+		
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
